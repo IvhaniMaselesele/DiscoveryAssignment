f7dd0badb1e21dae207b44b87b424306047acd96
ivhani <Ivhani.Maselesele@bsg.co.za>
2016-04-06 10:51:18 +0200

    First commit

---
+++ src/main/java/za/co/discovery/controllers/ShortestPathController.java
@@ -0,0 +1,12 @@
+package za.co.discovery.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShortestPathController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
