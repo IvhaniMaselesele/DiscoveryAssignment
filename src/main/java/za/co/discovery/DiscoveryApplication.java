cea592c8ad8f9d749b4594a00c299f220d1be2db
ivhani <Ivhani.Maselesele@bsg.co.za>
2016-04-06 15:48:25 +0200

    Fix yml file

--- src/main/java/za/co/discovery/DiscoveryApplication.java
+++ src/main/java/za/co/discovery/DiscoveryApplication.java
@@ -1,12 +1,25 @@
 package za.co.discovery;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
