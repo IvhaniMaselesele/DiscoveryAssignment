0e839915ac4d1ff5fb46a574cddeaeb27afdc76b
ivhani <Ivhani.Maselesele@bsg.co.za>
2016-04-06 11:05:34 +0200

    Test that root returns index page

---
+++ src/test/java/za/co/discovery/controllers/ShortestPathControllerTest.java
@@ -0,0 +1,35 @@
+package za.co.discovery.controllers;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class ShortestPathControllerTest {
+    MockMvc mockMvc;
+    
+    @Test
+    public void rootReturnsIndexPage() throws Exception {
+        setupFixture();
+        mockMvc.perform(get("/"))
+                .andExpect(view().name("index"));
+    }
+
+    public void setupFixture(){
+        mockMvc = standaloneSetup(
+                new ShortestPathController())
+                .setViewResolvers(getInternalResourceViewResolver())
+                .build();
+    }
+
+    private InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setSuffix(".html");
+        return viewResolver;
+    }
+}
