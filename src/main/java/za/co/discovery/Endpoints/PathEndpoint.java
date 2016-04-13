1757323e119c6a2616c58a93e5708cac57c26304
ivhani <Ivhani.Maselesele@bsg.co.za>
2016-04-13 15:36:25 +0200

    Add web service request

--- src/main/java/za/co/discovery/Endpoints/PathEndpoint.java
+++ src/main/java/za/co/discovery/Endpoints/PathEndpoint.java
@@ -1,7 +1,17 @@
 package za.co.discovery.Endpoints;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import za.co.discovery.Assignment.GetPathRequest;
+import za.co.discovery.Assignment.GetPathResponse;
+import za.co.discovery.repository.PathRepository;
+
+@Endpoint
 public class PathEndpoint {
-/*
+
     private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
 
     private PathRepository pathRepository;
@@ -11,14 +21,13 @@ public class PathEndpoint {
         this.pathRepository = pathRepository;
     }
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getPathRequest")
     @ResponsePayload
     public GetPathResponse getCountry(@RequestPayload GetPathRequest request) {
         GetPathResponse response = new GetPathResponse();
-        response.setPath(pathRepository.findVertex(request.getName()));
+        response.setPath(pathRepository.findPath(request.getName()));
 
         return response;
     }
 
-*/
 }
--- src/main/java/za/co/discovery/repository/PathRepository.java
+++ src/main/java/za/co/discovery/repository/PathRepository.java
@@ -1,14 +1,35 @@
 package za.co.discovery.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.util.Assert;
+import za.co.discovery.Assignment.Path;
+import za.co.discovery.Models.Graph;
+import za.co.discovery.Models.Vertex;
+import za.co.discovery.services.DijkstraAlgorithm;
+import za.co.discovery.services.FileReadingService;
+import za.co.discovery.services.GraphService;
+import za.co.discovery.services.ShortestPathService;
+
+import javax.annotation.PostConstruct;
+import java.util.LinkedList;
 
 @Component
 public class PathRepository {
-/*
+
     private GraphService graphService;
     private FileReadingService fileReadingService;
     private ShortestPathService shortestPathService;
     private Graph graph;
+    @Autowired
+    @Qualifier("transactionManager")
+    protected PlatformTransactionManager platformTransactionManager;
+
 
     @Autowired
     public PathRepository(FileReadingService fileReadingService, ShortestPathService shortestPathService, GraphService graphService) {
@@ -19,15 +40,32 @@ public class PathRepository {
 
     @PostConstruct
     public void initData() {
+        TransactionTemplate tpl = new TransactionTemplate(platformTransactionManager);
+        tpl.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                graph = graphService.createNewGraph();
+                fileReadingService.readPlanetSheet(graph);
+                fileReadingService.readRouteAndTrafficSheets(graph);
+            }
+        });
 
-        graph = graphService.createNewGraph();
-        fileReadingService.readPlanetSheet(graph);
-        fileReadingService.readRouteAndTrafficSheets(graph);
 
     }
 
-    public Vertex findVertex(String vertexID) {
+    public Path findPath(String vertexID) {
         Assert.notNull(vertexID);
-        return graph.getVertexById(vertexID);
-    }*/
+
+        Vertex vertex = graph.getVertexById(vertexID);
+        DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
+        dijkstra.execute(graph.getVertexes().get(0));
+        LinkedList<Vertex> pathList = dijkstra.getPath(vertex);
+
+        Path path = new Path();
+        path.setName("Path from:");
+        path.setDestination(vertex.getName());
+        path.setOrigin("Earth");
+        path.setPathList(shortestPathService.convertPathToString(pathList));
+        return path;
+    }
 }
