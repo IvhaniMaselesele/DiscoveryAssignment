0dae68977c23f626823359d17cd7e95212d55f5f
ivhani <Ivhani.Maselesele@bsg.co.za>
2016-04-13 13:23:31 +0200

    Get planet from user and calculate shortest path

---
+++ src/main/java/za/co/discovery/Endpoints/PathEndpoint.java
@@ -0,0 +1,24 @@
+package za.co.discovery.Endpoints;
+
+public class PathEndpoint {
+/*
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    private PathRepository pathRepository;
+
+    @Autowired
+    public PathEndpoint(PathRepository pathRepository) {
+        this.pathRepository = pathRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetPathResponse getCountry(@RequestPayload GetPathRequest request) {
+        GetPathResponse response = new GetPathResponse();
+        response.setPath(pathRepository.findVertex(request.getName()));
+
+        return response;
+    }
+
+*/
+}
--- src/main/java/za/co/discovery/repository/PathRepository.java
+++ src/main/java/za/co/discovery/repository/PathRepository.java
@@ -1,19 +1,10 @@
 package za.co.discovery.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
-import za.co.discovery.Models.Graph;
-import za.co.discovery.Models.Vertex;
-import za.co.discovery.services.FileReadingService;
-import za.co.discovery.services.GraphService;
-import za.co.discovery.services.ShortestPathService;
-
-import javax.annotation.PostConstruct;
 
 @Component
 public class PathRepository {
-
+/*
     private GraphService graphService;
     private FileReadingService fileReadingService;
     private ShortestPathService shortestPathService;
@@ -38,5 +29,5 @@ public class PathRepository {
     public Vertex findVertex(String vertexID) {
         Assert.notNull(vertexID);
         return graph.getVertexById(vertexID);
-    }
+    }*/
 }
