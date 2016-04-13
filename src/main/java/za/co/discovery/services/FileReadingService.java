36b4811ec8662ed339dc768bc11343fa075450bf
ivhani <Ivhani.Maselesele@bsg.co.za>
2016-04-13 14:35:39 +0200

    Update FileReadingService and FileReadingServiceTest

--- src/main/java/za/co/discovery/services/FileReadingService.java
+++ src/main/java/za/co/discovery/services/FileReadingService.java
@@ -1,7 +1,175 @@
 package za.co.discovery.services;
 
-/**
- * Created by Ivhani.Maselesele on 2016/04/07.
- */
-public class fileReadingService {
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.co.discovery.Models.Graph;
+import za.co.discovery.Models.Planet;
+import za.co.discovery.Models.Route;
+import za.co.discovery.dataAccess.PlanetDAO;
+import za.co.discovery.dataAccess.RouteDAO;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+@Service
+public class FileReadingService {
+    PlanetDAO planetDAO;
+    RouteDAO routeDAO;
+    ShortestPathService shortestPathService;
+
+    @Autowired
+    public FileReadingService(PlanetDAO planetDAO, RouteDAO routeDAO, ShortestPathService shortestPathService) {
+        this.shortestPathService = shortestPathService;
+        this.planetDAO = planetDAO;
+        this.routeDAO = routeDAO;
+    }
+
+    public Planet persistPlanet(Planet planet) {
+        return planetDAO.save(planet);
+    }
+
+    private Route getRouteRowValues(Iterator<Cell> cellIterator) {
+        String destination = "";
+        double distance = 0;
+        String origin = "";
+        int id = -1;
+        while (cellIterator.hasNext()) {
+            Cell cell = cellIterator.next();
+            switch (cell.getCellType()) {
+                case Cell.CELL_TYPE_NUMERIC:
+                    if (cell.getColumnIndex() == 3) distance = cell.getNumericCellValue();
+                    else if (cell.getColumnIndex() == 0) id = (int) cell.getNumericCellValue();
+
+                    break;
+                case Cell.CELL_TYPE_STRING:
+                    if (cell.getColumnIndex() == 1) origin = cell.getStringCellValue();
+                    else if (cell.getColumnIndex() == 2) destination = cell.getStringCellValue();
+                    break;
+            }
+        }
+        if (id != -1) {
+            Route route = new Route(id, origin, destination, distance);
+            return route;
+        }
+        return null;
+    }
+
+    public void readRouteAndTrafficSheets(Graph graph) {
+        try {
+            String fileName = new File("./").getCanonicalPath() + "\\src\\main\\java\\za\\co\\discovery\\dataObjects\\planetTravelDetails.xlsx";
+            FileInputStream file = new FileInputStream(fileName);
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            XSSFSheet sheet = workbook.getSheetAt(1);
+
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                Route route = getRouteRowValues(cellIterator);
+                if (route != null) {
+                    persistRoute(route);
+                }
+            }
+            file.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        getTraffic(graph);
+    }
+
+    public Route persistRoute(Route route) {
+        return routeDAO.save(route);
+    }
+
+    public Route updateRoute(Route route) {
+        return routeDAO.update(route);
+    }
+
+    public void readPlanetSheet(Graph graph) {
+        try {
+            String fileName = new File("./").getCanonicalPath() + "\\src\\main\\java\\za\\co\\discovery\\dataObjects\\planetTravelDetails.xlsx";
+            FileInputStream file = new FileInputStream(fileName);
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Planet planet;
+                String name = "";
+                String node = "";
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+                        case Cell.CELL_TYPE_STRING:
+                            if (cell.getColumnIndex() == 1) name = cell.getStringCellValue();
+                            else if (cell.getColumnIndex() == 0) node = cell.getStringCellValue();
+                            break;
+                    }
+                }
+
+                if (!name.toLowerCase().contains("name")) {
+                    planet = new Planet(node, name);
+                    if (!planet.getNode().equalsIgnoreCase("")) {
+                        persistPlanet(planet);
+                        shortestPathService.addNode(planet, graph);
+                    }
+                }
+            }
+            file.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getTraffic(Graph graph) {
+        try {
+            String fileName = new File("./").getCanonicalPath() + "\\src\\main\\java\\za\\co\\discovery\\dataObjects\\planetTravelDetails.xlsx";
+            FileInputStream file = new FileInputStream(fileName);
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(2);
+
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+                Route route = getRouteRowValues(cellIterator);
+
+                if (route != null) {
+                    double traffic = route.getDistance();
+                    int id = route.getId();
+                    Route retrievedRoute = getRouteById(id);
+                    retrievedRoute.setTraffic(traffic);
+                    updateRoute(retrievedRoute);
+                    shortestPathService.addEdge(retrievedRoute, graph);
+                }
+            }
+            file.close();
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+    }
+
+    private Route getRouteById(int id) {
+        return routeDAO.retrieve(id);
+    }
 }
+
--- src/test/java/za/co/discovery/services/FileReadingServiceTest.java
+++ src/test/java/za/co/discovery/services/FileReadingServiceTest.java
@@ -1,10 +1,46 @@
 package za.co.discovery.services;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import za.co.discovery.Models.Planet;
+import za.co.discovery.Models.Route;
+import za.co.discovery.dataAccess.PlanetDAO;
+import za.co.discovery.dataAccess.RouteDAO;
 
-/**
- * Created by Ivhani.Maselesele on 2016/04/07.
- */
-public class fileReadingServiceTest {
+import static com.shazam.shazamcrest.MatcherAssert.assertThat;
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
+
+public class FileReadingServiceTest {
+    FileReadingService fileReadingService;
+    @Mock
+    PlanetDAO planetDAO;
+    @Mock
+    RouteDAO routeDAO;
+
+    @Before
+    public void init() {
+        fileReadingService = new FileReadingService(planetDAO, routeDAO, new ShortestPathService());
+    }
+
+    @Test
+    public void persistPlanetShouldSavePlanet_AndReturnResult() {
+        //TODO : use a builder
+        Planet expectedPlanet = new Planet("random node", "random name");
+        when(planetDAO.save(expectedPlanet)).thenReturn(expectedPlanet);
+        Planet actualPlanet = fileReadingService.persistPlanet(expectedPlanet);
+        assertThat(actualPlanet, is(sameBeanAs(expectedPlanet)));
+    }
+
+    @Test
+    public void persistRouteShouldSaveRoute_AndReturnResult() {
+        Route expectedRoute = new Route(1, "random origin", "random destination", 0.2);
+        when(routeDAO.save(expectedRoute)).thenReturn(expectedRoute);
+        Route actualRoute = fileReadingService.persistRoute(expectedRoute);
+        assertThat(actualRoute, is(sameBeanAs(expectedRoute)));
+    }
+
 
 }
